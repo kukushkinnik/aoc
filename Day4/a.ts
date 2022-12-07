02ed954a60745080140b007551fbbc4efa012db5
Nikita <kukushkinnika@gmail.com>
2022-12-07 13:34:05 -0500

    changed some variables names and added types

--- Day4/a.ts
+++ Day4/a.ts
@@ -1,30 +1,34 @@
 import * as fs from "fs";
 
-const input = fs
+const input: string[][][] = fs
   .readFileSync("input.txt", "utf-8")
   .split("\n")
   .map((elem) => elem.split(","))
   .map((elem) => elem.map((el) => el.split("-")));
 
-const sectionsRanges = input.map((elem) => elem.map((elm) => createRange(elm)));
-const isSectionCoverdByOther = sectionsRanges.map((elem) => coversAll(elem));
+const sectionsRanges: number[][][] = input.map((elem) =>
+  elem.map((elm) => createRange(elm))
+);
+const isSectionCoverdByOther: boolean[] = sectionsRanges.map((elem) =>
+  coversAll(elem)
+);
 
-const amountOfOverLapingSections = isSectionCoverdByOther.filter(
+const amountOfOverLapingSections: boolean[] = isSectionCoverdByOther.filter(
   (el) => el === true
 );
 console.log(amountOfOverLapingSections.length);
 
-function createRange(arr: string[]) {
-  const [start, end] = arr;
-  const sections = [...Array(parseInt(end) - parseInt(start) + 1).keys()].map(
-    (x) => x + parseInt(start)
-  );
+function createRange(ranges: string[]) {
+  const [start, end] = ranges;
+  const sections: number[] = [
+    ...Array(parseInt(end) - parseInt(start) + 1).keys(),
+  ].map((x) => x + parseInt(start));
   return sections;
 }
 
 function coversAll(sections: number[][]) {
   const [firstElf, secondElf] = sections;
-  const results =
+  const results: boolean =
     secondElf.every((el) => firstElf.includes(el)) ||
     firstElf.every((el) => secondElf.includes(el));
   return results;
--- Day4/b.ts
+++ Day4/b.ts
@@ -1,29 +1,33 @@
 import * as fs from "fs";
 
-const input = fs
+const input: string[][][] = fs
   .readFileSync("input.txt", "utf-8")
   .split("\n")
   .map((elem) => elem.split(","))
   .map((elem) => elem.map((el) => el.split("-")));
 
-const sectionsRanges = input.map((elem) => elem.map((elm) => createRange(elm)));
-const isSectionCoverdByOther = sectionsRanges.map((elem) => coversAll(elem));
-const amountOfOverLapingSections = isSectionCoverdByOther.filter(
-  (el) => el === true
+const sectionsRanges: number[][][] = input.map((elem) =>
+  elem.map((elm) => createRange(elm))
+);
+const isSectionCoverdByOther: boolean[] = sectionsRanges.map((elem) =>
+  coversAll(elem)
 );
 
+const amountOfOverLapingSections: boolean[] = isSectionCoverdByOther.filter(
+  (el) => el === true
+);
 console.log(amountOfOverLapingSections.length);
 
-function createRange(arr: string[]) {
-  const [start, end] = arr;
-  const sections = [...Array(parseInt(end) - parseInt(start) + 1).keys()].map(
-    (x) => x + parseInt(start)
-  );
+function createRange(ranges: string[]) {
+  const [start, end] = ranges;
+  const sections: number[] = [
+    ...Array(parseInt(end) - parseInt(start) + 1).keys(),
+  ].map((x) => x + parseInt(start));
   return sections;
 }
 
 function coversAll(sections: number[][]) {
   const [firstElf, secondElf] = sections;
-  const results = secondElf.some((el) => firstElf.includes(el));
+  const results: boolean = secondElf.some((el) => firstElf.includes(el));
   return results;
 }
