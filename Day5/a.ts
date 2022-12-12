85f6b2b5b535bd8306189be5e375ab4e185d9c49
Nikita <kukushkinnika@gmail.com>
2022-12-12 13:30:10 -0500

    Day5 part 2 solved

--- Day5/a.ts
+++ Day5/a.ts
@@ -5,15 +5,14 @@ const inputStacks = input[0].split("\n");
 const movesInput = input[1].trim().split("\n");
 const stacks = [];
 
-
 //deleting stacks numbers
 inputStacks.pop();
 
 for (let stackLine of inputStacks) {
-  for (let i = 0; i < stackLine.length; i+= 4) {
+  for (let i = 0; i < stackLine.length; i += 4) {
     let start = i;
     let end = start + 3;
-    let crate = stackLine.substring(start,end);
+    let crate = stackLine.substring(start, end);
     let stackIndex = i / (3 + 1);
 
     if (!stacks[stackIndex]) {
@@ -30,21 +29,34 @@ const moves = movesInput
     .map(elem => elem.match(/\d{1,2}/g))
     .map(elem => elem.map(el => parseInt(el)));
 
-console.log(moves)
 
-for (let move of moves) {
-  let [amount, from, to] = move;
-  for(let i = 0; i < amount; i++) {
-    if(stacks[from-1] !== undefined) {
-      let crate = stacks[from-1].shift();
-      stacks[to-1].unshift(crate);
+const partOneResult = () => {
+  for (let move of moves) {
+    let [amount, from, to] = move;
+    for (let i = 0; i < amount; i++) {
+      if (stacks[from - 1] !== undefined) {
+        let crate = stacks[from - 1].shift();
+        stacks[to - 1].unshift(crate);
+      }
     }
   }
+
+  return stacks.map(stack => stack[0]).join("");
 }
 
-console.log(stacks.map(stack => stack[0]).join(""));
+const partTwoResult = () => {
+  for (let move of moves) {
+    let [amount, from, to] = move;
+    if (stacks[from - 1] !== undefined) {
+      let crateStack = stacks[from - 1].splice(0, amount);
+      stacks[to - 1].unshift(...crateStack)
+    }
+  }
 
+  return stacks.map(stack => stack[0]).join("");
+}
 
-
+console.log(partOneResult())
+console.log(partTwoResult());
 
 
